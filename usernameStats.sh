7e79517a72aff37dc5e11dbf0082c2c2019126b2
majorbonghits420 <jcc384@cornell.edu>
2020-05-11 15:04:06 -0500

    Add individual percentage, standardized output, add help in usage

--- usernameStats.sh
+++ usernameStats.sh
@@ -1,13 +1,18 @@
 #! /bin/bash
 
 usage() {
-    echo "Usage: $0 [-n <num> [-t]] [-f <filename>]";
+    echo "Usage: $0 [-n <num> [-t]] [-f <filename>] [-p]";
+    echo "-n <num>         Print the top <num> usernames"
+    echo "-t               Prints the percentage of attempts top <num> usernames are"
+    echo "-f <filename>    Set file, default /var/log/btmp"
+    echo "-p               Prints the percentage of failure next to number of attempts"
     exit 1;
 }
 num=""
 file="/var/log/btmp"
 totalPercentage=false
-while getopts ":n:f:t" option ; do
+singlePercentage=false
+while getopts ":n:f:tp" option ; do
     case "${option}" in
         n)
             if [ $OPTARG -eq $OPTARG ] 2>/dev/null; then
@@ -28,6 +33,9 @@ while getopts ":n:f:t" option ; do
         t)
             totalPercentage=true
             ;;
+        p)
+            singlePercentage=true
+            ;;
         *)
             usage
             ;;
@@ -47,7 +55,7 @@ fi
 
 # the "head -n -2" is to remove the last two lines of output, because lastb finishes
 # with an empty line and a line describing when the log begins
-baseOptions="| head -n -2 | awk '{ print \$1 }' | sort | uniq -c | sort -gr"
+baseOptions="| head -n -2 | awk '{ print \$1 }' | sort | uniq -c | sort -gr | awk '{ print \$1 \"\t\" \$2 } '"
 output=$(eval "lastb -f $file $baseOptions $trailer")
 
 # If -t is passed with -n, we compute the percentage the top n username are in failed attempts
@@ -59,4 +67,9 @@ if [ $totalPercentage = true ]; then
     echo $percentageStat
 fi
 
+# we calculate the single percentage for each user
+if [ $singlePercentage = true ]; then
+    attempts=$(lastb -f ${file} | head -n -2 | wc -l)
+    output=$(echo "${output}" | awk " { per = \$1 / $attempts * 100; print \$1,  per \"%\" \"\t\" \$2} ")
+fi
 echo "${output}"

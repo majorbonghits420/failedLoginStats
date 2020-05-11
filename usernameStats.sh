ea20e81432b13a6a65475e707278123ed3676752
majorbonghits420 <jcc384@cornell.edu>
2020-05-11 13:32:17 -0500

    Move to better iteration of username stats and parse cmdline options

--- usernameStats.sh
+++ usernameStats.sh
@@ -1,12 +1,41 @@
 #! /bin/bash
 
-# usage ./usernameStats.sh [-n #] <btmp file>
-baseOptions=" | awk '{ print \$1 }' | sort | uniq -c | sort -gr"
-trailer=""
-if [ $# -eq 3 ]; then
-    file=$3
-    trailer="| head -n $2"
-elif [ $# -eq 1 ]; then
-    file=$1
+usage() {
+    echo "Usage: $0 [-n <num>] [-f <filename>]";
+    exit 1;
+}
+num=""
+file="/var/log/btmp"
+while getopts ":n:f:" option ; do
+    case "${option}" in
+        n)
+            if [ $OPTARG -eq $OPTARG ] 2>/dev/null; then
+                num=${OPTARG}
+            else
+                echo "$0: Invalid number: $OPTARG"
+                usage
+            fi
+            ;;
+        f)
+            if [ -f $OPTARG ]; then
+                file=${OPTARG}
+            else
+                echo "$0: Cannot open $OPTARG: No such file"
+                usage
+            fi
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+if [ -z "$num" ]; then
+    trailer=""
+else
+    trailer="| head -n $num"
 fi
+# the "head -n -2" is to remove the last two lines of output, because lastb finishes
+# with an empty line and a line describing when the log begins
+baseOptions="| head -n -2 | awk '{ print \$1 }' | sort | uniq -c | sort -gr"
 eval "lastb -f $file $baseOptions $trailer"

974818cea751e68ec11e5d0a93e6f63ebfee7626
majorbonghits420 <jcc384@cornell.edu>
2020-08-12 15:42:35 -0500

    Add printing total percentage for outputed entries

--- ipStats.py
+++ ipStats.py
@@ -6,12 +6,15 @@
 import argparse
 import geoip2.database
 import socket
+import sys
 
 # create our parser, add options for getting the list of IPs and the DB file location
 argParser = argparse.ArgumentParser()
-argParser.add_argument('-f', '--ipList', required=True, help="The file with the list of IPs")
-argParser.add_argument('-d', '--database', required=True, help="GeoLite2-City Database file")
+argParser.add_argument('-f', '--ipList', required=True, help="The file with the list of IPs", type=str)
+argParser.add_argument('-d', '--database', required=True, help="GeoLite2-City Database file", type=str)
 argParser.add_argument('-p', '--percentage', required=False, help="Print country percentage of false logins", action="store_true")
+argParser.add_argument('-n', '--lines', required=False, help="Print first NUM lines of output", type=int)
+argParser.add_argument('-t', '--total', required=False, help="Prints total percentage printed entries account for", action="store_true")
 args = argParser.parse_args()
 
 # create reader object for the database
@@ -38,9 +41,24 @@ with open(args.ipList, 'r') as ipFile:
 print("Country Attempts Percentage")
 # Sort the dictionary based on value, descending order
 sortedCunts = {k : v for k, v in sorted(cunts.items(), key = lambda item: item[1], reverse=True)}
+linesToPrint=len(sortedCunts)
+if args.lines is not None:
+    if args.lines < 0:
+        print("Lines to print cannot be less than zero")
+        sys.exit(1)
+    linesToPrint=min(args.lines, linesToPrint)
+printedAttempts=0
+printedLines = linesToPrint # save for displaying later
 for key, value in sortedCunts.items():
     if args.percentage:
         print("{:<4} : {:>5} ({:>.2f}%) ".format(key, value, value / totalAttempts * 100))
     else:
         print("{:<4} : {:>5}".format(key, value, value / totalAttempts * 100))
+    printedAttempts += value
+    linesToPrint -= 1
+    if linesToPrint <= 0:
+        break;
+
+if args.total:
+    print("The top {} countries produced {:.2f}% of all failed attempts".format(printedLines, printedAttempts / totalAttempts * 100))
 print("There were {} invalid IPs in the file".format(invalidIPs))
